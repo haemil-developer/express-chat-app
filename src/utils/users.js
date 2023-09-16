6ef4c29bca3d44c2214f6532e2d5d98c15bb6850
haemil <every.haemil@gmail.com>
2023-09-17 00:49:58 +0900

    leave chat room

--- src/utils/users.js
+++ src/utils/users.js
@@ -26,17 +26,26 @@ const addUser = ({ id, username, room }) => {
     return { user };
 }
 
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
 const getUsersInRoom = (room) => {
     room = room.trim()
     return users.filter(user => user.room === room);
 }
 
-const getUser = (id) => {
-    return users.find((user) => user.id === id)
-}
-
 module.exports = {
     addUser,
+    getUser,
+    removeUser,
     getUsersInRoom,
-    getUser
 }
