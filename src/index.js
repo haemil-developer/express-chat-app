4365468019e6fa658a125d880c7faddca59c9027
haemil <every.haemil@gmail.com>
2023-09-16 22:57:03 +0900

    connect client & server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
 
-const app = express();
+const app = express();  // create an express app
+
+const server = http.createServer(app); // create server with express app
+const io = new Server(server);
 
 const publicDirectoryPath = '../public';
 app.use(express.static(path.join(__dirname, publicDirectoryPath)));
 
+io.on('connection', (socket => {
+    console.log(socket.id);
+
+    socket.on('join', () => {});
+
+    socket.on('sendMessage', () => {});
+
+    socket.on('discounnect', () => {});
+}))
+
 const port = 4000;
-app.listen(port, () => {
+server.listen(port, () => { // run express & socket.io
     console.log(`Server is up on port ${port}`);
-})
+});
